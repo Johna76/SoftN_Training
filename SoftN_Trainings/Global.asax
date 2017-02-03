0c29e7d5a0a83fb47b901bf096f79f48215bd3d1
ASUS_JOHNA\Johna <jonathan.fievet99@gmail.com>
2017-02-03 10:53:17 +0100

    Starting project

---
+++ SoftN_Trainings/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SoftN_Trainings.MvcApplication" Language="C#" %>
---
+++ SoftN_Trainings/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SoftN_Trainings.Startup))]
+namespace SoftN_Trainings
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
